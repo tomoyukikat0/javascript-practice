193c36bff3574d77de9c37c3b646a5533ec14ec7
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-07-01 00:32:56 +0900

    closure

---
+++ 030_スコープ/050_クロージャー（プライベート変数）/start/main.js
@@ -0,0 +1,20 @@
+let num = 0;
+
+function incrementFactory() {
+
+  let num = 0;
+
+  function increment() {
+    num = num + 1;
+    console.log(num);
+  }
+
+  return increment;
+}
+
+const increment = incrementFactory();
+
+increment();
+increment();
+increment();
+increment();
